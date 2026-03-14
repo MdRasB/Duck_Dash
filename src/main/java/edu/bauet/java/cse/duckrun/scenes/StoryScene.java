bdf2cfd91b7ada6fe1f39a96e9c20212c42b117a
aa-jim <abdullahaljim2@gmail.com>
2026-03-14 16:12:18 +0600

    counter font changed

--- src/main/java/edu/bauet/java/cse/duckrun/scenes/StoryScene.java
+++ src/main/java/edu/bauet/java/cse/duckrun/scenes/StoryScene.java
@@ -1,20 +1,21 @@
 package edu.bauet.java.cse.duckrun.scenes;
 
 import edu.bauet.java.cse.duckrun.MainApp;
+import edu.bauet.java.cse.duckrun.utils.AssetLoader;
 import javafx.animation.PauseTransition;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.media.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-
-import java.net.URL;
 
 public class StoryScene {
 
@@ -29,24 +30,25 @@ public class StoryScene {
 
         try {
 
-            URL videoUrl = getClass().getResource("/Story/opening.mp4");
+            // Load video using AssetLoader
+            Media video = AssetLoader.loadVideo("/Story/opening.mp4");
 
-            if (videoUrl == null) {
-                throw new RuntimeException("Video not found!");
+            if (video == null) {
+                throw new RuntimeException("Video not found");
             }
 
-            Media video = new Media(videoUrl.toExternalForm());
             mp = new MediaPlayer(video);
 
             MediaView mv = new MediaView(mp);
             mv.setFitWidth(MainApp.WINDOW_WIDTH);
+            mv.setFitHeight(MainApp.WINDOW_HEIGHT);
             mv.setPreserveRatio(true);
 
             root.getChildren().add(mv);
 
             mp.setAutoPlay(true);
 
-            final MediaPlayer finalMp = mp;
+            MediaPlayer finalMp = mp;
             mp.setOnEndOfMedia(() -> navigateToMenu(stage, finalMp));
 
         } catch (Exception e) {
@@ -58,7 +60,8 @@ public class StoryScene {
         skipLabel = new Label("Press SPACE to Skip");
 
         Font pixelFont = Font.loadFont(
-                getClass().getResourceAsStream("/fonts/PressStart2P-Regular.ttf"), 12);
+                getClass().getResourceAsStream("/fonts/PressStart2P-Regular.ttf"), 12
+        );
 
         if (pixelFont != null) {
             skipLabel.setFont(pixelFont);
@@ -75,7 +78,7 @@ public class StoryScene {
         root.getChildren().add(skipLabel);
 
         StackPane.setAlignment(skipLabel, Pos.BOTTOM_RIGHT);
-        StackPane.setMargin(skipLabel, new Insets(0,20,20,0));
+        StackPane.setMargin(skipLabel, new Insets(0, 20, 20, 0));
 
         Scene scene = new Scene(root, MainApp.WINDOW_WIDTH, MainApp.WINDOW_HEIGHT);
 
