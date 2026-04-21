2f1a582c203a602253c3889801127da71aab7456
aa-jim <abdullahaljim2@gmail.com>
2026-04-21 11:56:00 +0600

    duck shadow update

--- src/main/java/edu/bauet/java/cse/duckrun/entities/Duck.java
+++ src/main/java/edu/bauet/java/cse/duckrun/entities/Duck.java
@@ -203,8 +203,7 @@ public class Duck {
             duckView.setImage(toggleFrame
                     ? (sleepy ? duckingImageSleepy         : duckingImage)
                     : (sleepy ? duckingMidPointImageSleepy : duckingMidPointImage));
-            duckView.setLayoutY(groundLine - DISPLAY_HEIGHT + 20);
-            duckShadow.setLayoutY(groundLine - DISPLAY_HEIGHT + 43);
+            duckView.setLayoutY(groundLine - DISPLAY_HEIGHT + 15);
 
             // Play step sound on each waddle frame while crouching
             if (frameJustToggled) {
