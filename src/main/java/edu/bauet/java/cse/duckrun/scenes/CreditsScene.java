867d01e22ea7f1eb1288a8223c961fa9af23a137
MdRasB <muhammadrasekb@gmail.com>
2026-05-03 19:33:26 +0600

    Update Java version in CreditsScene

--- src/main/java/edu/bauet/java/cse/duckrun/scenes/CreditsScene.java
+++ src/main/java/edu/bauet/java/cse/duckrun/scenes/CreditsScene.java
@@ -244,7 +244,7 @@ public class CreditsScene {
         addSpacer(col, 50);
 
         addToolGroup(col, "Language & Runtime",
-                "Java 21   ·   JDK 21.0.10");
+                "Java 21   ·   JDK 17+");
         addSpacer(col, 50);
         addToolGroup(col, "Framework & UI",
                 "JavaFX 21",
