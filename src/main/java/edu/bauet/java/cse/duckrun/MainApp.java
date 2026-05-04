cf8b3bd7ab12fd70594abe38d94fe69318c3b9db
Shabab <shababnandigram28@gmail.com>
2026-05-04 15:52:06 +0600

    window starts maximized

--- src/main/java/edu/bauet/java/cse/duckrun/MainApp.java
+++ src/main/java/edu/bauet/java/cse/duckrun/MainApp.java
@@ -32,6 +32,7 @@ public class MainApp extends Application {
         Scene scene = storyScene.createScene(stage);
 
         primaryStage.setScene(scene);
+        primaryStage.setMaximized(true);
         primaryStage.show();
     }
 
